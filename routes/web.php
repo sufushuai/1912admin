b8f8c47e480671ff58d6a36b694a259962a34c16
lin <1501113246@qq.com>
2020-09-17 16:14:24 +0800

    web

--- routes/web.php
+++ routes/web.php
@@ -20,21 +20,6 @@ Route::get('/admin/login','Admin\LoginController@login');//登录
 Route::post('/admin/logindo','Admin\LoginController@logindo');//执行登录
 
 
-Route::get('admin/category','Admin\CateController@category');//分类添加
-Route::get('admin/cate/index','Admin\CateController@index');//分类展示
-Route::any('/admin/users','Admin\UserController@users');//管理员添加
-Route::any('/admin/users/index','Admin\UserController@index');//管理员展示
-
-Route::any('/admin/role','Admin\RoleController@role');//角色添加
-Route::any('/admin/role/index','Admin\RoleController@index');//角色展示
-
-Route::any('/admin/based','Admin\BasedController@based');//权限添加
-Route::any('/admin/based/index','Admin\BasedController@index');//权限展示
-
-Route::get('admin/category','Admin\CateController@category');//分类添加
-Route::get('admin/cate/index','Admin\CateController@index');//分类展示
-
-
 Route::any('cate/create','Admin\CateController@create');//分类添加
 Route::any('cate/store','Admin\CateController@store');//添加逻辑
 Route::any('cate/delete','Admin\CateController@delete');//软删除
@@ -47,9 +32,6 @@ Route::any('/admin/cate/index','Admin\CateController@index');//分类展示
 Route::any('/admin/brand','Admin\BrandController@brand');//品牌添加页面
 Route::any('/brand/add','Admin\BrandController@add');//品牌添加
 Route::any('/admin/brandimg','Admin\BrandController@brandimg');//图片添加
-
-Route::any('/admin/brand','Admin\BrandController@brand');//品牌添加
-Route::any('/admin/brandimg','Admin\BrandController@brandimg');//品牌添加
 Route::any('/admin/brand/index','Admin\BrandController@index');//品牌展示
 
 Route::any('/admin/vip','Admin\VipController@vip');//vip添加
